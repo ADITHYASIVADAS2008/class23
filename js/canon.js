ad66d04703455fb3ec6184f602bcdba4a47caa56
ADITHYASIVADAS2008 <85430440+ADITHYASIVADAS2008@users.noreply.github.com>
2021-08-28 17:14:39 +0530

    Add files via upload

---
+++ js/canon.js
@@ -0,0 +1,28 @@
+class Canon {
+constructor(x,y,width,height,angle)
+
+{
+this.x =x;
+this.y =y;
+this.width  = width;
+this.height = height;
+this.angle = angle 
+
+
+}
+
+display(){
+
+fill("grey");
+push()
+translate(this.x,this.y);
+rotate(this.angle);
+rect(-10,-20,this.width,this.height);
+pop ()
+arc(this.x-30,this.y+18,180,230,PI,TWO_PI);
+noFill
+    
+}
+
+
+}
